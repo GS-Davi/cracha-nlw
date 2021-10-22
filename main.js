327fd148542aa08a6f0717885e163d3e9b9930cb
Davi Souza <55307900+DaavSouza@users.noreply.github.com>
2021-10-22 18:06:19 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,54 @@
+const LinksSocialMedia = {
+  github: 'DaavSouza',
+  youtube: 'channel/UCguKVFVGEqp1fhg5nac3REA',
+  facebook: 'Davi.Souza45',
+  instagram: 'davi.gsouza',
+  twitter: 'davssouza_'
+}
+
+function changeSocialMediaLinks() {
+  for (let li of socialLinks.children) {
+    const social = li.getAttribute('class')
+
+    li.children[0].href = `https://${social}.com/${LinksSocialMedia[social]}`
+  }
+}
+
+changeSocialMediaLinks()
+
+function getGitHubProfileInfos() {
+  const url = `https://api.github.com/users/${LinksSocialMedia.github}`
+
+  fetch(url)
+    .then(response => response.json())
+    .then(data => {
+      userName.textContent = data.name
+      userBio.textContent = data.bio
+      userLink.href = data.html_url
+      userImage.src = data.avatar_url
+      userLogin.textContent = data.login
+    })
+}
+
+getGitHubProfileInfos()
+
+// JavaScript
+
+// Variaveis: let ou const
+// let = Para dados alternados
+// const = constante (Para dados fixos)
+
+// Objeto guarda diversas informações dentro de uma
+// variável
+
+// Quando um erro acontece em JS normalmente está
+// descrito no Console
+
+// camelCase
+// PascalCase
+// snake_case
+
+// Arrow Functions é uma forma contraida de função
+// elimina a necessidade da função ter um nome
+// você só passa o argumento =>
+// porém para rodar a função é mais difícil
