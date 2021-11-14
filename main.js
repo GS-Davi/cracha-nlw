21cb8e192b3d2519c2e3e22c5123cb32bb861733
Davi Souza <55307900+GS-Davi@users.noreply.github.com>
2021-11-13 23:04:12 -0300

    Update main.js

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-const LinksSocialMedia = {
+const linksSocialMedia = {
   github: 'GS-Davi',
   youtube: 'channel/UCguKVFVGEqp1fhg5nac3REA',
   facebook: 'Davi.Souza45',
@@ -10,14 +10,14 @@ function changeSocialMediaLinks() {
   for (let li of socialLinks.children) {
     const social = li.getAttribute('class')
 
-    li.children[0].href = `https://${social}.com/${LinksSocialMedia[social]}`
+    li.children[0].href = `https://${social}.com/${linksSocialMedia[social]}`
   }
 }
 
 changeSocialMediaLinks()
 
 function getGitHubProfileInfos() {
-  const url = `https://api.github.com/users/${LinksSocialMedia.github}`
+  const url = `https://api.github.com/users/${linksSocialMedia.github}`
 
   fetch(url)
     .then(response => response.json())
